dfb48e6a6593333ba9ee6df15444bc61513dc8e2
James Maher <jamespmaher@gmail.com>
2022-06-10 14:36:48 +0100

    Added README.md and Provision_VM.ps1 files

---
+++ Provision_VM.ps1
@@ -0,0 +1,37 @@
+# New VM In PowerShell:
+
+# Declare Variables Here From User Input.
+#
+$VMName = Read-Host -Prompt "Enter VM Name: "
+$Ram = Read-Host -Prompt "Enter Ram Required(gb): "
+#$VMPath = Read-Host -Prompt "Enter Path to Store VM: "
+$OSDrive = Read-Host -Prompt "Enter OS Drive Size(gb): "
+$DataDrive = Read-Host -Prompt "Enter Data Drive Size(gb): "
+$ISOPath = Read-Host -Prompt "Enter Path to ISO: "
+$vSwitch = Read-Host -Prompt "Enter vSwitch Name: "
+$vCPU = Read-host -Prompt "Enter Number of vCPUs: "
+
+
+# Create New VM
+New-VM -Name $VMName -MemoryStartupBytes $Ram -Generation 2 -SwitchName $vSwitch -NewVHDPath "E:\Hyper-V\Virtual Hard Disks\$VMName\C_DRIVE.vhdx" -NewVHDSizeBytes $OSDrive
+
+#Remove Existing DVD Drive.
+#Remove-VMDvdDrive -VMName "$VMName" -ControllerNumber 1 -ControllerLocation 0
+
+# Add DVD Drive and Path to ISO.
+Get-VM $VMName | Add-VMDvdDrive -Path "$ISOPath"
+
+# Add a Data Drive.
+
+if (-not ($DataDrive -eq $null)){
+     # do your processing here
+     New-VHD -Path "E:\Hyper-V\Virtual Hard Disks\$VMName\E_DRIVE.vhdx" -SizeBytes $DataDrive -Dynamic
+     Add-VMHardDiskDrive -VMName "$VMName" -Path "E:\Hyper-V\Virtual Hard Disks\$VMName\E_DRIVE.vhdx"
+ }
+
+
+# Set Proccesor Count vCPU
+Set-VM -Name $VMName -ProcessorCount $vCPU
+
+$OsDvdDrive = Get-VMDvdDrive -VMName $VMName -ControllerNumber 0
+Set-VMFirmware -VMName $VMName -FirstBootDevice $OSDvdDrive 
---
+++ README.md
@@ -0,0 +1,1 @@
+##This is a basic PowerShell Script to add a virtual machine in Hyper-V
